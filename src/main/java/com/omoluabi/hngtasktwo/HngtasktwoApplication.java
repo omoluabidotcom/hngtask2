f8175cb207a7074d5a71dd87a475ee2ea0e87dbf
omoluabidotcom <yyahaya222@yahoo.com>
2022-11-05 10:53:42 +0100

    initial commit

---
+++ src/main/java/com/omoluabi/hngtasktwo/HngtasktwoApplication.java
@@ -0,0 +1,13 @@
+package com.omoluabi.hngtasktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngtasktwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HngtasktwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omoluabi/hngtasktwo/HngtasktwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omoluabi.hngtasktwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngtasktwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
