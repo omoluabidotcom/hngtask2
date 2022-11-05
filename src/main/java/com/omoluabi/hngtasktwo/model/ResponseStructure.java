954272b5750da194ca6ef18d7f23b47af3410c64
omoluabidotcom <yyahaya222@yahoo.com>
2022-11-05 20:31:04 +0100

    response structure class add

--- src/main/java/com/omoluabi/hngtasktwo/model/ResponseStructure.java
+++ src/main/java/com/omoluabi/hngtasktwo/model/ResponseStructure.java
@@ -1,4 +1,45 @@
 package com.omoluabi.hngtasktwo.model;
 
-public class DetailsStructure {
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseStructure {
+
+    String slackUsername;
+    Integer result;
+    ArithmeticOperationType operation_type;
+
+    public ResponseStructure() {}
+
+    public ResponseStructure(String slackUsername, Integer result, ArithmeticOperationType operation_type) {
+
+        this.slackUsername = slackUsername;
+        this.result = result;
+        this.operation_type = operation_type;
+    }
+
+    public void setSlackUsername(String slackUsername) {
+        this.slackUsername = slackUsername;
+    }
+
+    public String getSlackUsername() {
+        return slackUsername;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setOperation_type(ArithmeticOperationType operation_type) {
+        this.operation_type = operation_type;
+    }
+
+    public ArithmeticOperationType getOperation_type() {
+        return operation_type;
+    }
+
 }
