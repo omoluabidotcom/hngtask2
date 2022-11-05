64643653f2a48425560bc915b22d56bb944a7512
omoluabidotcom <yyahaya222@yahoo.com>
2022-11-05 20:30:30 +0100

    request structure class

--- src/main/java/com/omoluabi/hngtasktwo/model/RequestStructure.java
+++ src/main/java/com/omoluabi/hngtasktwo/model/RequestStructure.java
@@ -1,2 +1,15 @@
-package com.omoluabi.hngtasktwo.model;public class OperationStructure {
+package com.omoluabi.hngtasktwo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestStructure {
+
+    String operation_type;
+    Integer x;
+    Integer y;
 }
