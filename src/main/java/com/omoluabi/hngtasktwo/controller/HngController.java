1c8193e778a16ff000b2f71aa6ac70f9af118e1b
omoluabidotcom <yyahaya222@yahoo.com>
2022-11-05 20:31:38 +0100

    controller complete

--- src/main/java/com/omoluabi/hngtasktwo/controller/HngController.java
+++ src/main/java/com/omoluabi/hngtasktwo/controller/HngController.java
@@ -1,9 +1,21 @@
 package com.omoluabi.hngtasktwo.controller;
 
+import com.omoluabi.hngtasktwo.model.RequestStructure;
+import com.omoluabi.hngtasktwo.model.ResponseStructure;
+import com.omoluabi.hngtasktwo.service.ArithmeticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HngController {
 
+    @Autowired
+    private ArithmeticService arithmeticService;
 
+    @PostMapping("/arithmetic")
+    public ResponseStructure arithmeticOperation(@RequestBody RequestStructure requestStructure) {
+        return arithmeticService.performOperation(requestStructure);
+    }
 }
